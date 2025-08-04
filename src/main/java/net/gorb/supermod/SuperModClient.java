a0180872deb87ae2b2fa7fa7c8c1483ab735f3b9
Gorb <aekaraosmanoglu@gmail.com>
2025-08-04 16:06:13 +0300

    starting

---
+++ src/main/java/net/gorb/supermod/SuperModClient.java
@@ -0,0 +1,8 @@
+package net.gorb.supermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
---
+++ src/main/java/net/gorb/supermod/SuperModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gorb.supermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SuperModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
